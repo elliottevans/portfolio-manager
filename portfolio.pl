5a3163606c73e6419f1e4c0a78518dd8317d595f
elliottevans <evanselliott@verizon.net>
2014-11-08 13:44:00 -0600

    added css and perl files we'll use

---
+++ portfolio.pl
@@ -0,0 +1,162 @@
+#!/usr/bin/perl -w
+
+
+# database input and output is paired into the two arrays noted
+
+my @sqlinput=();
+my @sqloutput=();
+
+# The combination of -w and use strict enforces various 
+# rules that make the script more resilient and easier to run
+# as a CGI script.
+#
+use strict;
+
+# The CGI web generation stuff
+# This helps make it easy to generate active HTML content
+# from Perl
+#
+# We'll use the "standard" procedural interface to CGI
+# instead of the OO default interface
+use CGI qw(:standard);
+
+# The interface to the database.  The interface is essentially
+# the same no matter what the backend database is.  
+#
+# DBI is the standard database interface for Perl. Other
+# examples of such programatic interfaces are ODBC (C/C++) and JDBC (Java).
+#
+#
+# This will also load DBD::Oracle which is the driver for
+# Oracle.
+use DBI;
+
+#
+#
+# A module that makes it easy to parse relatively freeform
+# date strings into the unix epoch time (seconds since 1970)
+#
+use Time::ParseDate;
+
+
+#
+# You need to override these for access to your database
+#
+my $dbuser="ebe213";
+my $dbpasswd="z52oQZjbp";
+
+# Will be filled in as we process the paramters
+
+my $user = undef;
+my $password = undef;
+
+#
+# Get the user action and whether he just wants the form or wants us to
+# run the form
+#
+my $action;
+my $run;
+
+if (defined(param("act"))) { 
+  $action=param("act");
+  if (defined(param("run"))) { 
+    $run = param("run") == 1;
+  } else {
+    $run = 0;
+  }
+} else {
+  $action="base";
+  $run = 1;
+}
+
+#
+# Now we finally begin generating back HTML
+#
+#
+#print start_html('Red, White, and Blue');
+print "<html style=\"height: 100\%\">";
+print "<head>";
+print "<title>Red, White, and Blue</title>";
+print "</head>";
+
+print "<body style=\"height:100\%;margin:0\">";
+
+#
+# Force device width, for mobile phones, etc
+#
+#print "<meta name=\"viewport\" content=\"width=device-width\" />\n";
+
+# This tells the web browser to render the page in the style
+# defined in the css file
+#
+print "<style type=\"text/css\">\n\@import \"rwb.css\";\n</style>\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################################################
+#
+# Nothing important after this
+#
+######################################################################
+
+# The following is necessary so that DBD::Oracle can
+# find its butt
+#
+BEGIN {
+  unless ($ENV{BEGIN_BLOCK}) {
+    use Cwd;
+    $ENV{ORACLE_BASE}="/raid/oracle11g/app/oracle/product/11.2.0.1.0";
+    $ENV{ORACLE_HOME}=$ENV{ORACLE_BASE}."/db_1";
+    $ENV{ORACLE_SID}="CS339";
+    $ENV{LD_LIBRARY_PATH}=$ENV{ORACLE_HOME}."/lib";
+    $ENV{BEGIN_BLOCK} = 1;
+    exec 'env',cwd().'/'.$0,@ARGV;
+  }
+}
