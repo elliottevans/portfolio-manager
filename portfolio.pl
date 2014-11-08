d3644ebad260e368fc08354c7a233a770dac17bb
elliottevans <evanselliott@verizon.net>
2014-11-08 17:03:43 -0600

    updated portfolio.pl. Now it's a skeleton for our web app

--- portfolio.pl
+++ portfolio.pl
@@ -1,11 +1,50 @@
 #!/usr/bin/perl -w
 
+#
+#
+# portfolio.pl
+#
+#
+# Project B code for EECS 339, Northwestern University
+# 
+# 
 
+
+
+#						Elliott Evans
+#						Tim Sullivan
+#						11/8/2014				
+#
+
+# The overall theory of operation of this script is as follows
+#
+# 1. The inputs are form parameters, if any, and a session cookie, if any. 
+# 2. The session cookie contains the login credentials (User/Password).
+# 3. The parameters depend on the form, but all forms have the following three
+#    special parameters:
+#
+#         act      =  form  <the form in question> (form=base if it doesn't exist)
+#         run      =  0 Or 1 <whether to run the form or not> (=0 if it doesn't exist)
+#         debug    =  0 Or 1 <whether to provide debugging output or not> 
+#
+# 4. The script then generates relevant html based on act, run, and other 
+#    parameters that are form-dependent
+# 5. The script also sends back a new session cookie (allowing for logout functionality)
+# 6. The script also sends back a debug cookie (allowing debug behavior to propagate
+#    to child fetches)
+#
+
+
+#
+# Debugging
+#
 # database input and output is paired into the two arrays noted
-
+#
+my $debug=0; # default - will be overriden by a form parameter or cookie
 my @sqlinput=();
 my @sqloutput=();
 
+#
 # The combination of -w and use strict enforces various 
 # rules that make the script more resilient and easier to run
 # as a CGI script.
@@ -19,6 +58,7 @@ use strict;
 # We'll use the "standard" procedural interface to CGI
 # instead of the OO default interface
 use CGI qw(:standard);
+
 
 # The interface to the database.  The interface is essentially
 # the same no matter what the backend database is.  
@@ -39,16 +79,45 @@ use DBI;
 use Time::ParseDate;
 
 
+
 #
 # You need to override these for access to your database
 #
 my $dbuser="ebe213";
 my $dbpasswd="z52oQZjbp";
 
-# Will be filled in as we process the paramters
 
+#
+# The session cookie will contain the user's name and password so that 
+# he doesn't have to type it again and again. 
+#
+# "RWBSession"=>"user/password"
+#
+# BOTH ARE UNENCRYPTED AND THE SCRIPT IS ALLOWED TO BE RUN OVER HTTP
+# THIS IS FOR ILLUSTRATION PURPOSES.  IN REALITY YOU WOULD ENCRYPT THE COOKIE
+# AND CONSIDER SUPPORTING ONLY HTTPS
+#
+my $cookiename="RWBSession";
+#
+# And another cookie to preserve the debug state
+#
+my $debugcookiename="RWBDebug";
+
+#
+# Get the session input and debug cookies, if any
+#
+my $inputcookiecontent = cookie($cookiename);
+my $inputdebugcookiecontent = cookie($debugcookiename);
+
+#
+# Will be filled in as we process the cookies and paramters
+#
+my $outputcookiecontent = undef;
+my $outputdebugcookiecontent = undef;
+my $deletecookie=0;
 my $user = undef;
 my $password = undef;
+my $logincomplain=0;
 
 #
 # Get the user action and whether he just wants the form or wants us to
@@ -56,6 +125,7 @@ my $password = undef;
 #
 my $action;
 my $run;
+
 
 if (defined(param("act"))) { 
   $action=param("act");
@@ -69,6 +139,126 @@ if (defined(param("act"))) {
   $run = 1;
 }
 
+my $dstr;
+
+if (defined(param("debug"))) { 
+  # parameter has priority over cookie
+  if (param("debug") == 0) { 
+    $debug = 0;
+  } else {
+    $debug = 1;
+  }
+} else {
+  if (defined($inputdebugcookiecontent)) { 
+    $debug = $inputdebugcookiecontent;
+  } else {
+    # debug default from script
+  }
+}
+
+$outputdebugcookiecontent=$debug;
+
+#
+#
+# Who is this?  Use the cookie or anonymous credentials
+#
+#
+if (defined($inputcookiecontent)) { 
+  # Has cookie, let's decode it
+  ($user,$password) = split(/\//,$inputcookiecontent);
+  $outputcookiecontent = $inputcookiecontent;
+} else {
+  # No cookie, treat as anonymous user
+  ($user,$password) = ("anon","anonanon");
+}
+
+#
+# Is this a login request or attempt?
+# Ignore cookies in this case.
+#
+if ($action eq "login") { 
+  if ($run) { 
+    #
+    # Login attempt
+    #
+    # Ignore any input cookie.  Just validate user and
+    # generate the right output cookie, if any.
+    #
+    ($user,$password) = (param('user'),param('password'));
+    if (ValidUser($user,$password)) { 
+      # if the user's info is OK, then give him a cookie
+      # that contains his username and password 
+      # the cookie will expire in one hour, forcing him to log in again
+      # after one hour of inactivity.
+      # Also, land him in the base query screen
+      $outputcookiecontent=join("/",$user,$password);
+      $action = "base";
+      $run = 1;
+    } else {
+      # uh oh.  Bogus login attempt.  Make him try again.
+      # don't give him a cookie
+      $logincomplain=1;
+      $action="login";
+      $run = 0;
+    }
+  } else {
+    #
+    # Just a login screen request, but we should toss out any cookie
+    # we were given
+    #
+    undef $inputcookiecontent;
+    ($user,$password)=("anon","anonanon");
+  }
+} 
+
+
+#
+# If we are being asked to log out, then if 
+# we have a cookie, we should delete it.
+#
+if ($action eq "logout") {
+  $deletecookie=1;
+  $action = "base";
+  $user = "anon";
+  $password = "anonanon";
+  $run = 1;
+}
+
+
+my @outputcookies;
+
+#
+# OK, so now we have user/password
+# and we *may* have an output cookie.   If we have a cookie, we'll send it right 
+# back to the user.
+#
+# We force the expiration date on the generated page to be immediate so
+# that the browsers won't cache it.
+#
+if (defined($outputcookiecontent)) { 
+  my $cookie=cookie(-name=>$cookiename,
+		    -value=>$outputcookiecontent,
+		    -expires=>($deletecookie ? '-1h' : '+1h'));
+  push @outputcookies, $cookie;
+} 
+#
+# We also send back a debug cookie
+#
+#
+if (defined($outputdebugcookiecontent)) { 
+  my $cookie=cookie(-name=>$debugcookiename,
+		    -value=>$outputdebugcookiecontent);
+  push @outputcookies, $cookie;
+}
+
+#
+# Headers and cookies sent back to client
+#
+# The page immediately expires so that it will be refetched if the
+# client ever needs to update it
+#
+print header(-expires=>'now', -cookie=>\@outputcookies);
+
 #
 # Now we finally begin generating back HTML
 #
@@ -76,7 +266,7 @@ if (defined(param("act"))) {
 #print start_html('Red, White, and Blue');
 print "<html style=\"height: 100\%\">";
 print "<head>";
-print "<title>Red, White, and Blue</title>";
+print "<title>Portfolio</title>";
 print "</head>";
 
 print "<body style=\"height:100\%;margin:0\">";
@@ -90,54 +280,433 @@ print "<body style=\"height:100\%;margin:0\">";
 # defined in the css file
 #
 print "<style type=\"text/css\">\n\@import \"rwb.css\";\n</style>\n";
+  
+
+print "<center>" if !$debug;
+
+
+#
+#
+# The remainder here is essentially a giant switch statement based
+# on $action. 
+#
+#
+#
+
+
+# LOGIN
+#
+# Login is a special case since we handled running the filled out form up above
+# in the cookie-handling code.  So, here we only show the form if needed
+# 
+#
+if ($action eq "login") { 
+  if ($logincomplain) { 
+    print "Login failed.  Try again.<p>"
+  } 
+  if ($logincomplain or !$run) { 
+    print start_form(-name=>'Login'),
+      h2('Login to Red, White, and Blue'),
+	"Name:",textfield(-name=>'user'),	p,
+	  "Password:",password_field(-name=>'password'),p,
+	    hidden(-name=>'act',default=>['login']),
+	      hidden(-name=>'run',default=>['1']),
+		submit,
+		  end_form;
+  }
+}
 
 
 
+#
+# BASE
+#
+# The base action presents the overall page to the browser
+# This is the "document" that the JavaScript manipulates
+#
+#
+if ($action eq "base") { 
+  
+  #Make the checkboxes for filtering data.
+  
+  print "<form><input type = \"checkbox\" id = \"candidate_check\" onclick = 'ViewShift()'>check box example</input></form>";
+  
+  
+  
+  #
+  #
+  # And something to color (Red, White, or Blue)
+  #
+  print "<div id=\"color\" style=\"width:100\%; height:10\%\"></div>";
+ 
+  
+
+  #
+  # User mods
+  #
+  #  
 
 
 
+  if ($user eq "anon") {
+    print "<p>You are anonymous, but you can also <a href=\"rwb.pl?act=login\">login</a></p>";
+  } else {
+    print "<p>You are logged in as $user and can do the following:</p>";
+    if (UserCan($user,"give-opinion-data")) {
+      print "<p><a href=\"rwb.pl?act=give-opinion-data\">Give Opinion Of Current Location</a></p>";
+    }
+    if (UserCan($user,"give-cs-ind-data")) {
+      print "<p><a href=\"rwb.pl?act=give-cs-ind-data\">Geolocate Individual Contributors</a></p>";
+    }    
+    print "<p><a href=\"rwb.pl?act=logout&run=1\">Logout</a></p>";
+  }
+
+}
+
+
+#
+#
+# NEAR
+#
+#
+# Nearby committees, candidates, individuals, and opinions
+#
+#
+# Note that the individual data should integrate the FEC data and the more
+# precise crowd-sourced location data.   The opinion data is completely crowd-sourced
+#
+# This form intentionally avoids decoration since the expectation is that
+# the client-side javascript will invoke it to get raw data for overlaying on the map
+#
+#
+
+my $total_dem_money = 0;
+my $total_rep_money = 0;
+my $opinion_average = 0;
+my $opinion_deviation = 0;
+
+if ($action eq "near") {
+ 
+
+  my $latne = param("latne");
+  my $longne = param("longne");
+  my $latsw = param("latsw");
+  my $longsw = param("longsw");
+  my $whatparam = param("what");
+  my $format = param("format");
+  my $cycle = param("cycle");  
+  my %what;
+    
+  $format = "table" if !defined($format);
+  $cycle = "1112" if !defined($cycle);
+
+  
+
+  if (!defined($whatparam) || $whatparam eq "all") { 
+    %what = ( committees => 1, 
+	      candidates => 1,
+	      individuals =>1,
+	      opinions => 1);
+  } else {
+    map {$what{$_}=1} split(/\s*,\s*/,$whatparam);
+  }
+	       
+
+  if ($what{committees}) { 
+     my ($str,$error) = Committees($latne,$longne,$latsw,$longsw,$cycle,$format);
+     my @dem_com_money = calc_dem_com_money($latne,$longne,$latsw,$longsw,$cycle,$format);
+     my @rep_com_money = calc_rep_com_money($latne,$longne,$latsw,$longsw,$cycle,$format);
+     my $democratic_committee_money = $dem_com_money[0];
+     my $republican_committee_money = $rep_com_money[0];     
+     $total_dem_money += $democratic_committee_money; 
+     $total_rep_money += $republican_committee_money;    
+     
+    if (!$error) {
+      if ($format eq "table") { 
+	print "<h2>Nearby committees</h2>$str";
+      } else {
+	print $str;
+      }
+    }
+  }
+    
+
+print "<p>Opinion Std. Deviation:</p>";
+print "<div id = 'opinion_deviation'>$opinion_deviation</div>";
+
+}
+
+
+#
+# INVITE-USER
+#
+# Invite User functionality
+#
+#
+#
+#
+ 
+
+
+#
+#This creates the page the invited user signs up with.
+#
+
+if ($action eq "sign-up") { 
+    
+    if (!$run) { 
+      print h2("Sign Up for RWB");  
+  print start_form(-name=>'InviteUser'),
+	h2('Enter Your Info and You Will be in the System'),	  
+	      "Username: ", textfield(-name=>'user'),
+		p,		  
+              "Password: ", textfield(-name=>'password'),
+                p,              
+              "Email: ", textfield(-name=>'email'),
+                p,
+              "User Perms Copied/Pasted from Email: ", textfield(-name=>'permissions'),
+                p,  
+               "User who invited you: ", textfield(-name=>'referrer'),
+                p, 
+		      hidden(-name=>'run',-default=>['1']),
+			hidden(-name=>'act',-default=>['invite-user']),
+			  submit,
+			    end_form,
+			      hr;
+    } else {
+        my $user =  param('user');
+        my $password = param('password');
+        my $permissions = param('permissions');
+        my $referrer = param('referrer');
+        my $email = param('email');
+        UserAdd($user,$password,$email,$referrer);
+        $permissions =~ s/\s//g; #remove white space
+        my @perms = split(',', $permissions); #split perms at commas 
+        foreach (@perms) {
+          #give the user whatever he was granted
+          GiveUserPerm($user, $_);
+        }       
+	print "$user is now in the system.\n\n";
+        print "<p><a href=\"rwb.pl?act=login\">Login</a></p>";      
+    
+  }  
+}
 
 
 
+#
+# GIVE-OPINION-DATA 
+# 
+# Give Opinion Data functionality
+#
+#
+#
+#
+
+if ($action eq "give-opinion-data") { 
+  if (!UserCan($user,"add-users") && !UserCan($user,"manage-users")) { 
+    print h2('You do not have the required permissions to add users.');
+  } else {
+    if (!$run) {
+
+
+      #couldn't get these variables to take the parameters from rwb.js. See the 
+      #start function in rwb.pl for more.
+      my $mylat = param("mylat");
+      my $mylong = param("mylong");
+      
+      
+      print start_form(-name=>'GiveOpinionData'),
+	h2('Give Opinion Data for Current Location'),	  
+	      "Latitude: ", textfield(-name=>'latitude'), " Currently: 42.06108",
+		p,		  
+              "Longitude: ", textfield(-name=>'longitude'), " Currently: -87.67855",              
+                p,
+              "Color: ", textfield(-name=>'color'), 
+                p,
+              "-1 (red) through 0 (white) to +1 (blue)",
+                p,
+		      hidden(-name=>'run',-default=>['1']),
+			hidden(-name=>'act',-default=>['invite-user']),
+			  submit,
+			    end_form,
+			      hr;
+    } else {
+        my $entered_latitude = param('latitude');
+        my $entered_longitude = param('longitude');
+        my $color = param('color');
+        eval { ExecSQL($dbuser,$dbpasswd,
+		 "insert into rwb_opinions values ('$user',$color,$entered_latitude,$entered_longitude)");};
+  
+        print "Added opinion data color: $color for ($entered_latitude, $entered_longitude)";             
+    }
+  }
+  print "<p><a href=\"rwb.pl?act=base&run=1\">Return</a></p>";
+}
+
+
+#
+#
+#
+#
+# Debugging output is the last thing we show, if it is set
+#
+#
+#
+#
+
+
+#
+# The main line is finished at this point. 
+# The remainder includes utilty and other functions
+#
+
+#calculate the color deviation
+sub calc_opinion_deviation {
+  my ($latne,$longne,$latsw,$longsw,$cycle,$format) = @_;
+  my @rows;
+  
+  my $command = "select stddev(color) from rwb_opinions where latitude < $latne and latitude > $latsw and longitude < $longne and longitude > $longsw";
+ 
+  eval {@rows = ExecSQL($dbuser, $dbpasswd, $command, "ROW");};
+}
 
 
 
+#
+# Generate a table of nearby opinions
+#
+# ($table|$raw,$error) = Opinions(latne,longne,latsw,longsw,cycle,format)
+# $error false on success, error string on failure
+#
+sub Opinions {
+  my ($latne, $longne, $latsw, $longsw, $cycle,$format) = @_;
+  my @rows;
+  eval { 
+    @rows = ExecSQL($dbuser, $dbpasswd, "select latitude, longitude, color from rwb_opinions where latitude>? and latitude<? and longitude>? and longitude<?",undef,$latsw,$latne,$longsw,$longne);
+  };
+  
+  if ($@) { 
+    return (undef,$@);
+  } else {
+    if ($format eq "table") { 
+      return (MakeTable("opinion_data","2D",
+			["latitude", "longitude", "name", "city", "state", "zip", "employer", "amount"],
+			@rows),$@);
+    } else {
+      return (MakeRaw("opinion_data","2D",@rows),$@);
+    }
+  }
+}
 
+#
+# Add a user
+# call with name,password,email
+#
+# returns false on success, error string on failure.
+# 
+# UserAdd($name,$password,$email)
+#
+sub UserAdd { 
+  eval { ExecSQL($dbuser,$dbpasswd,
+		 "insert into rwb_users (name,password,email,referer) values (?,?,?,?)",undef,@_);};
+  return $@;
+}
 
+#
+#
+# Check to see if user and password combination exist
+#
+# $ok = ValidUser($user,$password)
+#
+#
+sub ValidUser {
+  my ($user,$password)=@_;
+  my @col;
+  eval {@col=ExecSQL($dbuser,$dbpasswd, "select count(*) from rwb_users where name=? and password=?","COL",$user,$password);};
+  if ($@) { 
+    return 0;
+  } else {
+    return $col[0]>0;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#
+# @list=ExecSQL($user, $password, $querystring, $type, @fill);
+#
+# Executes a SQL statement.  If $type is "ROW", returns first row in list
+# if $type is "COL" returns first column.  Otherwise, returns
+# the whole result table as a list of references to row lists.
+# @fill are the fillers for positional parameters in $querystring
+#
+# ExecSQL executes "die" on failure.
+#
+sub ExecSQL {
+  my ($user, $passwd, $querystring, $type, @fill) =@_;
+  if ($debug) { 
+    # if we are recording inputs, just push the query string and fill list onto the 
+    # global sqlinput list
+    push @sqlinput, "$querystring (".join(",",map {"'$_'"} @fill).")";
+  }
+  my $dbh = DBI->connect("DBI:Oracle:",$user,$passwd);
+  if (not $dbh) { 
+    # if the connect failed, record the reason to the sqloutput list (if set)
+    # and then die.
+    if ($debug) { 
+      push @sqloutput, "<b>ERROR: Can't connect to the database because of ".$DBI::errstr."</b>";
+    }
+    die "Can't connect to database because of ".$DBI::errstr;
+  }
+  my $sth = $dbh->prepare($querystring);
+  if (not $sth) { 
+    #
+    # If prepare failed, then record reason to sqloutput and then die
+    #
+    if ($debug) { 
+      push @sqloutput, "<b>ERROR: Can't prepare '$querystring' because of ".$DBI::errstr."</b>";
+    }
+    my $errstr="Can't prepare $querystring because of ".$DBI::errstr;
+    $dbh->disconnect();
+    die $errstr;
+  }
+  if (not $sth->execute(@fill)) { 
+    #
+    # if exec failed, record to sqlout and die.
+    if ($debug) { 
+      push @sqloutput, "<b>ERROR: Can't execute '$querystring' with fill (".join(",",map {"'$_'"} @fill).") because of ".$DBI::errstr."</b>";
+    }
+    my $errstr="Can't execute $querystring with fill (".join(",",map {"'$_'"} @fill).") because of ".$DBI::errstr;
+    $dbh->disconnect();
+    die $errstr;
+  }
+  #
+  # The rest assumes that the data will be forthcoming.
+  #
+  #
+  my @data;
+  if (defined $type and $type eq "ROW") { 
+    @data=$sth->fetchrow_array();
+    $sth->finish();
+    if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","ROW",undef,@data);}
+    $dbh->disconnect();
+    return @data;
+  }
+  my @ret;
+  while (@data=$sth->fetchrow_array()) {
+    push @ret, [@data];
+  }
+  if (defined $type and $type eq "COL") { 
+    @data = map {$_->[0]} @ret;
+    $sth->finish();
+    if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","COL",undef,@data);}
+    $dbh->disconnect();
+    return @data;
+  }
+  $sth->finish();
+  if ($debug) {push @sqloutput, MakeTable("debug_sql_output","2D",undef,@ret);}
+  $dbh->disconnect();
+  return @ret;
+}
 
 
 ######################################################################
@@ -160,3 +729,4 @@ BEGIN {
     exec 'env',cwd().'/'.$0,@ARGV;
   }
 }
+
